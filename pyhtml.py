20ad327183b248dd9bb8f9db70c0defce033e8cf
UperscuzziSchoolAcc <111448336+UperscuzziSchoolAcc@users.noreply.github.com>
2022-08-22 10:30:07 -0500

    added more elements, development is now over

--- pyhtml.py
+++ pyhtml.py
@@ -13,6 +13,10 @@ def n(string):
 def f(string):
     f = open("index.html", "a")
     f.write(string + "\n")
+def doctype():
+    f("<!DOCTYPE html>")
+def end():
+    f("</html>")
 def e(l, string, style=""):
     return "<" + str(l) + " style=" + '"' + listToString(style) + '"' + ">" + str(string) + "<" + "/" + str(l) + ">"
 def ehref(l, string, href="", style=""):
@@ -51,13 +55,19 @@ def q(string, style=None):
     return e("q", string, style)
 def strong(string, style=None):
     return e("strong", string, style)
-def audio(string, href, style=None):
+def audio(string, href):
     return ehref("audio", string, href)
-def img(string, href, style=None):
+def img(string, href):
     return ehref("img", string, href)
-def video(string, href, style=None):
+def video(string, href):
     return ehref("video", string, href)
-def a(href, string, style=None):
+def a(href, string):
     return ehref("a", string, href)
-def button(href, string, style=None,):
+def button(href, string):
     return ehref("button", string, href)
+def address(string, style=None):
+    return e("address", string, style)
+def aside(string, style=None):
+    return e("aside", string, style)
+def footer(string, style=None):
+    return e("footer", string, style)
